23ffd62ef1ed41bd841cee3509b9b6b108c73b73
Alexander Boychenko <boichenko.alexander@gmail.com>
2021-06-08 15:52:26 +0700

    Apply suggestions from code review
    Co-authored-by: Søren Pedersen <sh.pedersen@gmail.com>

--- src/Likvido.Extensions.Logging/SerilogServiceCollectionExtensions.cs
+++ src/Likvido.Extensions.Logging/SerilogServiceCollectionExtensions.cs
@@ -35,7 +35,7 @@ namespace Likvido.Extensions.Logging
         /// <summary>
         /// Sets Serilog as the logging provider.
         /// </summary>
-        /// <param name="collection">The host builder to configure.</param>
+        /// <param name="collection">The service collection to configure.</param>
         /// <param name="logger">The Serilog logger; if not supplied, the static <see cref="Serilog.Log"/> will be used.</param>
         /// <param name="dispose">When <c>true</c>, dispose <paramref name="logger"/> when the framework disposes the provider. If the
         /// logger is not specified but <paramref name="dispose"/> is <c>true</c>, the <see cref="Serilog.Log.CloseAndFlush()"/> method will be
@@ -43,7 +43,7 @@ namespace Likvido.Extensions.Logging
         /// <param name="providers">A <see cref="LoggerProviderCollection"/> registered in the Serilog pipeline using the
         /// <c>WriteTo.Providers()</c> configuration method, enabling other <see cref="Microsoft.Extensions.Logging.ILoggerProvider"/>s to receive events. By
         /// default, only Serilog sinks will receive events.</param>
-        /// <returns>The host builder.</returns>
+        /// <returns>The service collection.</returns>
         public static IServiceCollection UseSerilog(
             this IServiceCollection collection,
             Serilog.ILogger logger = null,
@@ -71,7 +71,7 @@ namespace Likvido.Extensions.Logging
             }
             else
             {
-                collection.AddSingleton<ILoggerFactory>(services => new SerilogLoggerFactory(logger, dispose));
+                collection.AddSingleton<ILoggerFactory>(_ => new SerilogLoggerFactory(logger, dispose));
             }
 
             return collection;
@@ -82,13 +82,13 @@ namespace Likvido.Extensions.Logging
         /// A <see cref="IServiceCollection"/> is supplied so that configuration and hosting information can be used.
         /// The logger will be shut down when application services are disposed.
         /// </remarks>
-        /// <param name="builder">The host builder to configure.</param>
+        /// <param name="collection">The service collection to configure.</param>
         /// <param name="configureLogger">The delegate for configuring the <see cref="Serilog.LoggerConfiguration" /> that will be used to construct a <see cref="Serilog.Core.Logger" />.</param>
         /// <param name="preserveStaticLogger">Indicates whether to preserve the value of <see cref="Serilog.Log.Logger"/>.</param>
         /// <param name="writeToProviders">By default, Serilog does not write events to <see cref="ILoggerProvider"/>s registered through
         /// the Microsoft.Extensions.Logging API. Normally, equivalent Serilog sinks are used in place of providers. Specify
         /// <c>true</c> to write events to all providers.</param>
-        /// <returns>The host builder.</returns>
+        /// <returns>The service collection.</returns>
         public static IServiceCollection UseSerilog(
             this IServiceCollection collection,
             Action<Serilog.LoggerConfiguration> configureLogger,
@@ -115,10 +115,10 @@ namespace Likvido.Extensions.Logging
 
         /// <summary>Sets Serilog as the logging provider.</summary>
         /// <remarks>
-        /// A <see cref="HostBuilderContext"/> is supplied so that configuration and hosting information can be used.
+        /// A <see cref="IServiceCollection"/> is supplied so that configuration and hosting information can be used.
         /// The logger will be shut down when application services are disposed.
         /// </remarks>
-        /// <param name="builder">The host builder to configure.</param>
+        /// <param name="collection">The service collection to configure.</param>
         /// <param name="configureLogger">The delegate for configuring the <see cref="Serilog.LoggerConfiguration" /> that will be used to construct a <see cref="Serilog.Core.Logger" />.</param>
         /// <param name="preserveStaticLogger">Indicates whether to preserve the value of <see cref="Serilog.Log.Logger"/>.</param>
         /// <param name="writeToProviders">By default, Serilog does not write events to <see cref="ILoggerProvider"/>s registered through
@@ -128,7 +128,7 @@ namespace Likvido.Extensions.Logging
         /// <c>LoggerConfigurationExtensions.CreateBootstrapLogger()</c>), and <paramref name="preserveStaticLogger"/> is
         /// not specified, the the bootstrap logger will be reconfigured through the supplied delegate, rather than being
         /// replaced entirely or ignored.</remarks>
-        /// <returns>The host builder.</returns>
+        /// <returns>The service collection.</returns>
         public static IServiceCollection UseSerilog(
             this IServiceCollection collection,
             Action<IServiceProvider, LoggerConfiguration> configureLogger,
